d0916fc7a00988591e321d7bb1a13f8479c7974c
@omarcasasolasmerida <omarcasasolasmerida@gmail.com>
2021-05-04 08:39:35 -0400

    addUsser

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,8 +3,8 @@
         protected $connexion_bd;
         public function Conexion(){
             try {
-                // $this->connexion_bd = new PDO("pgsql:host=129.151.100.42;port=5432;dbname=cotizacion","omar","js77Hi1JhG76GKp");
-                $this->connexion_bd = new PDO("pgsql:host=localhost;port=5434;dbname=PruebaRolesUsuario","postgres","kirium");
+                $this->connexion_bd = new PDO("pgsql:host=129.151.100.42;port=5432;dbname=cotizacion","omar","js77Hi1JhG76GKp");
+                // $this->connexion_bd = new PDO("pgsql:host=localhost;port=5434;dbname=PruebaRolesUsuario","postgres","kirium");
                 //Lo mas importante
                 $this->connexion_bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $this->connexion_bd;
